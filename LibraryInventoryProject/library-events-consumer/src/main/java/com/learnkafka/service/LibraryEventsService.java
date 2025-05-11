7a9ffc3849cfd17caf1ff36ee46dde7e4de99f73
Bheemshekhar <bheemshekhar.golla@gmail.com>
2025-05-11 19:59:34 +0530

    Spring Kafka: Library Events Consumer

---
+++ LibraryInventoryProject/library-events-consumer/src/main/java/com/learnkafka/service/LibraryEventsService.java
@@ -0,0 +1,29 @@
+package com.learnkafka.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.learnkafka.entity.LibraryEvent;
+import com.learnkafka.repository.LibraryEventsRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class LibraryEventsService {
+
+    @Autowired
+    LibraryEventsRepository repository;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    public void processLibraryEvent(ConsumerRecord<Integer, String> consumerRecord) throws JsonProcessingException {
+        LibraryEvent libraryEvent = objectMapper.readValue(consumerRecord.value(), LibraryEvent.class);
+        log.info("Library Event :{}", libraryEvent);
+        //set LibraryEvent reference in Book Entity; one to one mapping
+        libraryEvent.getBook().setLibraryEvent(libraryEvent);
+        repository.save(libraryEvent);
+    }
+}
