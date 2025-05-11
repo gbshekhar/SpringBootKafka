94d34e63435d31f1756737faf18b26c75b3ef1d1
Bheemshekhar <bheemshekhar.golla@gmail.com>
2025-05-11 10:22:19 +0530

    Update Library Event RestEnd point

--- LibraryInventoryProject/library-events-producer/src/main/java/com/learnkafka/controller/LibraryEventsController.java
+++ LibraryInventoryProject/library-events-producer/src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -2,6 +2,7 @@ package com.learnkafka.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.learnkafka.domain.LibraryEvent;
+import com.learnkafka.domain.LibraryEventType;
 import com.learnkafka.producer.LibraryEventsProducer;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +34,29 @@ public class LibraryEventsController {
         libraryEventsProducer.sendLibraryEvents_Approach2(libraryEvent);
         log.info("After sending library-event");
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+
+    @PutMapping("/v1/libraryevent")
+    public ResponseEntity<?> updateLibraryEvent(@RequestBody @Valid LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
+        log.info("library event {}",libraryEvent);
+
+        ResponseEntity<String> BAD_REQUEST = validateLibraryEvent(libraryEvent);
+        if(BAD_REQUEST != null) return  BAD_REQUEST;
+
+        //invoke Kafka Producer
+        libraryEventsProducer.sendLibraryEvents_Approach2(libraryEvent);
+        return ResponseEntity.status(HttpStatus.OK).body(libraryEvent);
+    }
+
+    private static ResponseEntity<String> validateLibraryEvent(LibraryEvent libraryEvent){
+        //validation LibraryEventId should be not null
+        if(libraryEvent.libraryEventId() == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please pass the LibaryEventId");
+        }
+        if(!libraryEvent.libraryEventType().equals(LibraryEventType.UPDATE)){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Only update Event Type is supported");
+        }
+
+        return null;
     }
 }
